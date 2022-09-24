7f57a6476dc1291a7cf8c29615d98ffcffa56db3
Artur Didur <didur.art@gmail.com>
2022-09-24 13:04:47 +0300

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        boolean isA = false;
+        int a1 = 10;
+        char a2 = 'a';
+        byte a3 = 127;
+        short a4 = 32000;
+        long a5 =  1800000L;
+        float a6 = 1.01f;
+        double a7 = 1.09;
+
+        System.out.println(hello);
+        System.out.println(a1);
+        System.out.println(a6);
+    }
+}
